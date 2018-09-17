c574ed6df7e15f85bdb01b3a1315cfe19c579973
ooxiaoyan <810978527@qq.com>
2018-09-17 17:18:50 +0800

    Update UI in sub thread

--- app/src/main/java/com/example/androidthreadtest/MainActivity.java
+++ app/src/main/java/com/example/androidthreadtest/MainActivity.java
@@ -1,13 +1,56 @@
 package com.example.androidthreadtest;
 
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private TextView text;
+
+    private static final int UPDATE_TEXT = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        text = findViewById(R.id.text);
+        Button changeText = findViewById(R.id.change_text);
+        changeText.setOnClickListener(this);
+    }
+
+    private Handler handler = new Handler() {
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case UPDATE_TEXT:
+                    //在这里可以进行UI操作
+                    text.setText("Nice to meet you!");
+                    break;
+                default:
+                    break;
+            }
+        }
+    };
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.change_text:
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Message message = new Message();
+                        message.what = UPDATE_TEXT;
+                        handler.sendMessage(message);//将Message对象发送出去
+                    }
+                }).start();
+                break;
+            default:
+                break;
+        }
     }
 }
